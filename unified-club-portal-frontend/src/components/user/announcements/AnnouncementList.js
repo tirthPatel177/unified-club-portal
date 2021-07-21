718cd720367bf36aad2c9506bd8b102f9ea37ab5
Tirth Patel <177tpatel@gmail.com>
2021-07-21 11:44:41 +0530

    Accouncement Page User View

--- unified-club-portal-frontend/src/components/user/announcements/AnnouncementList.js
+++ unified-club-portal-frontend/src/components/user/announcements/AnnouncementList.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 import Header from '../clubs_profile/header/index'
 import Navbar from './../NavBar/Navbar'
 import AnnouncementCard from './AnnouncementCard'
@@ -6,43 +7,32 @@ import './AnnouncementList.css'
 
 const AnnouncementList = () => {
 
-    const data = [
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-        {
-            clubname: 'Programming Club',
-            subject: "Opportunity at MIT",
-            date: '26-12-2020'
-        },
-    ]
+    const [data, setdata] = useState([])
+
+    let {club} = useParams();
+
+    const fetchannouncements = (token) => {
+        let formData = new FormData();
+        formData.append("token", token);
+        formData.append("club_name", club.split('-').join(' '))
+        fetch('http://localhost:8000/api/club/get_announcements', {
+            method: "POST",
+            body: formData
+        }).then( dataapi => dataapi.json()).then(
+            dataapi => {
+                console.log(dataapi);
+                setdata(dataapi);
+            }
+        ).catch(e => console.log(e))
+    }
+    
+      useEffect(() => {
+        let token = localStorage.getItem('token');
+        fetchannouncements(token);
+        
+      }, [])
+
+    
 
     return (
         <div>
