3e57e90458075bab57f650fa260a04d33e1624ac
Nipun3333 <nipunpatel3333@gmail.com>
2021-07-24 22:02:28 +0530

    temp

--- unified-club-portal-backend/backendapi/api/admin.py
+++ unified-club-portal-backend/backendapi/api/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Book, Type_of_User, Club_profile, Event, Member, Register_Event, Announcement, Rating, event_view
+from .models import Type_of_User, Club_profile, Event, Member, Register_Event, Announcement, Rating, event_view
 
-admin.site.register(Book)
 admin.site.register(Type_of_User)
 admin.site.register(Club_profile)
 admin.site.register(Event)
